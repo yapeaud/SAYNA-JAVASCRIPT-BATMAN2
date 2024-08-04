05d9bcbe48c3814e6d828bf1868645cd52612d04
Yapeaud <yapoabed@gmail.com>
2024-08-04 02:28:19 +0200

    sayna javascript batman2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,5 @@
-// ACTIVATION DU MENU DE NAVIGATION
+/* CETTE FONCTION GÈRE LE COMPORTEMENT DES LIENS DE NAVIGATION DANS UNE BARRE DE NAVIGATION */
+
 document.addEventListener('DOMContentLoaded', (event) => { // Attend que le DOM soit entièrement chargé
     const links = document.querySelectorAll('.navbar a'); // Sélectionne tous les liens dans la barre de navigation
 
@@ -10,7 +11,8 @@ document.addEventListener('DOMContentLoaded', (event) => { // Attend que le DOM 
     });
 });
 
-//
+/* GERER L'INTERACTION AVEC LE BOUTON "Next Question" EN VÉRIFIANT QUE L'UTILISATEUR A SÉLECTIONNÉ AU MOINS UNE RÉPONSE AVANT DE PASSER À LA QUESTION SUIVANTE. */
+
 document.getElementById('nextQuestionBtn').addEventListener('click', function(event) { // Ajoute un écouteur d'événement pour le bouton 'nextQuestionBtn'
     const checkboxes = document.querySelectorAll('input[name="answer"]'); // Sélectionne toutes les cases à cocher avec le nom 'answer'
     let isChecked = false; // Initialise une variable pour vérifier si une case est cochée
